aa29aceee2cd8b97cecb9827a326f9511e091527
Adrienn Mahler <ajanka23@gmail.com>
2024-08-03 13:14:56 +0200

    Initial commit

---
+++ src/LocalDateK.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class LocalDate {
+    public static void main(String[] args) {
+
+
+        Date d = new Date();
+        System.out.println("d= " + d);
+        d.getDay();
+    }
+}
